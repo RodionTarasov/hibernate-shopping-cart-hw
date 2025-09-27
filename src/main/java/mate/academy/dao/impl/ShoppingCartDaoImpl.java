2f144fc1cc81b174df3dd9e4c53c7e331aaf5fa9
kozma <kozmakruckov@gmail.com>
2025-09-27 19:42:40 +0300

    added solution 1

---
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,47 @@
+package mate.academy.dao.impl;
+
+import jakarta.persistence.EntityManager;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Optional;
+
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert shopping cart" + shoppingCart, e);
+        }
+    }
+
+    @Override
+    public Optional<ShoppingCart> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.get(ShoppingCart.class, user.getId()));
+        }
+
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        EntityManager entityManager = HibernateUtil.getSessionFactory().getCurrentSession();
+        entityManager.merge(shoppingCart);
+    }
+}
